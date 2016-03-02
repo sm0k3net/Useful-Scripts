3c938d01edf90daabe16ad4cf856d71264995579
sm0k3 <sm0k3net@users.noreply.github.com>
2016-03-03 00:57:33 +0300

    Create winlogs.ps1

---
+++ winlogs.ps1
@@ -0,0 +1,31 @@
+#Parameters
+param(
+[String]$type,
+[Int32]$count=5
+)
+
+#Date
+$logsDate = Get-Date -UFormat "%Y%m%d"
+
+#Logs output path
+$logsOutput = "C:\Users\umurashka\Desktop\"+$logsDate+"_"+$type+".txt"
+
+
+#Showing & storing data
+if ($type -eq 'list') {
+	clear
+		echo 'Listing log types:'
+		$logs = Get-EventLog -List
+		echo $logs
+} 
+Else {
+	Clear-Content $logsOutput
+	clear
+	echo 'Storing logs into file...'
+		$logs = Get-EventLog -log $type -Newest $count
+			foreach($logItem in $logs) {
+				$logMsg = (($logItem | format-list -property *) | Out-String) -replace "\s+", " "
+				echo $logMsg >> $logsOutput
+			}
+		echo "Done!"
+}

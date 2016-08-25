29d6fd306ce5fb9c41ae42a54f78f9081feef7c1
sm0k3 <sm0k3net@users.noreply.github.com>
2016-08-25 21:16:56 +0300

    Create swap.sh

---
+++ swap.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#########################################################
+## This script is designed to automatically count RAM, ##
+## count size of possible swap and create swap file.   ##
+#########################################################
+
+clear
+
+RAM="`free -m | grep Mem | awk '{print $2}'`"
+swap_allowed=$(($RAM * 2))
+swap=$swap_allowed"M"
+
+echo -e "Detected RAM: $RAM
+Recommended swap size: $swap"
+echo -e "Create swap for: "
+read swap_size
+
+read -r -p "Are you sure you want to create swap for $swap_size? [y/N] " response
+case $response in
+    [yY][eE][sS]|[yY]) 
+
+  echo "Creating swap..."
+
+  fallocate -l $swap_size /var/swap.img
+  chmod 600 /var/swap.img
+  mkswap /var/swap.img
+  swapon /var/swap.img
+
+  echo -e "Swap for $swap_size was succesfully created!"
+  sleep 3
+
+  free -m | grep swap
+  swapon -s
+
+        ;;
+    *)
+        echo -e "Creation of swap file was rejected! Exiting!"
+        exit 0
+        ;;
+esac
+
+

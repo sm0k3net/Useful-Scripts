19fefd70ef3d7577485e0b00240be98903f78c85
sm0k3 <sm0k3net@users.noreply.github.com>
2017-12-21 00:41:23 +0300

    updated axfr.sh script
    Were added "dig" availability check in OS, sorting of the results, dumping successful transfers.

--- axfr.sh
+++ axfr.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-
-# how-to:
-# $ chmod +x axfr.sh
-# $ cat domains.txt | xargs ./axfr.sh
-# You will find separate files named by domain names and results of test inside
-
+if which dig 2>&1 >/dev/null ; then
+echo "dig installed!"
+mkdir "domains_check"
 for domain
 do
 	ns=$(dig ns $domain +short)
 	for item in $ns
 	do
 		axfr=$(dig axfr $domain @$item)
-		echo "$axfr" >> "$domain".txt
+		if test=$(echo "$axfr" | grep "Transfer failed") ;
+		then 
+			echo "$domain [$item] - failed" >> domains_check/stats.txt
+			#echo "true"
+		else
+			echo "$axfr" >> "domains_check/$domain".txt
+			echo "$domain [$item] - success" >> domains_check/stats.txt
+			#echo "false"
+		fi
 	done
 done
+
+else
+	echo "No dig installed..."
+	echo "Use 'apt-get install dnsutils' (Debian/Ubuntu) or 'yum install dnsutils' (CentOS/RHEL)"
+fi
